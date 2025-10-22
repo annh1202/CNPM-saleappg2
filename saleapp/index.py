13431600e172bd0968cb36b07ffe3cf6f9f49dc0
annh <2351050002an@ou.edu.vn>
2025-10-22 11:00:53 +0700

    create project

---
+++ saleapp/index.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import dao
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    q = request.args.get("q")
+    cate_id = request.args.get("cate_id")
+    cates = dao.load_category()
+    prods = dao.load_product(q=q, cate_id=cate_id)
+    return render_template("index.html", cates = cates, prods=prods)
+
+@app.route("/products/<int:id>")
+def details(id):
+
+    return render_template("product-details.html")
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
